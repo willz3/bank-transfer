aa18b9c042903b34d0fd12633715a34b8437dd56
Willian Vassilek <willz3@outlook.com>
2024-02-19 12:28:36 -0300

    test: should return a MerchantPayerError if the payer is a merchant.

--- src/test/java/com/example/banktransfer/module/transference/v1/usecase/CreateTransference/CreateTransferenceUseCaseTest.java
+++ src/test/java/com/example/banktransfer/module/transference/v1/usecase/CreateTransference/CreateTransferenceUseCaseTest.java
@@ -1,6 +1,9 @@
 package com.example.banktransfer.module.transference.v1.usecase.CreateTransference;
 
+import com.example.banktransfer.core.shared.logic.Either;
 import com.example.banktransfer.module.transference.v1.entity.TransferenceEntity;
+import com.example.banktransfer.module.transference.v1.entity.UserEntity;
+import com.example.banktransfer.module.transference.v1.error.MerchantPayerError;
 import com.example.banktransfer.module.transference.v1.gateway.user.IUserGateway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +14,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
@@ -22,17 +27,25 @@ class CreateTransferenceUseCaseTest {
     @InjectMocks
     CreateTransferenceUseCase sut;
 
+    private final Long PAYER_ID = 1L;
+    private final Long PAYEE_ID = 2L;
+    private final String SSN = "46101743080";
+    private final String ENI = "15964243000117";
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        when(userGateway.findUserById(PAYER_ID)).thenReturn(makeUser(PAYER_ID, UserEntity.UserType.COMMOM, SSN));
+        when(userGateway.findUserById(PAYEE_ID)).thenReturn(makeUser(PAYEE_ID, UserEntity.UserType.MERCHANT, ENI));
     }
+
 
     @Test
     @DisplayName("should call user gateway with correct values to find a payer.")
     void callUserGatewayPayerId() {
         TransferenceEntity transferenceEntity = makeEntity();
         sut.execute(transferenceEntity);
-        verify(userGateway, times(1)).findUserById(transferenceEntity.getPayerId());
+        verify(userGateway, times(1)).findUserById(PAYER_ID);
     }
 
     @Test
@@ -40,11 +53,29 @@ class CreateTransferenceUseCaseTest {
     void callUserGatewayWithPayeeId() {
         TransferenceEntity transferenceEntity = makeEntity();
         sut.execute(transferenceEntity);
-        verify(userGateway, times(1)).findUserById(transferenceEntity.getPayeeId());
+        verify(userGateway, times(1)).findUserById(PAYEE_ID);
+    }
+
+    @Test
+    @DisplayName("should return an error if the payer is a merchant")
+    void shouldReturnErrorIfPayerIsMerchant() {
+        TransferenceEntity transferenceEntity = makeEntity();
+        UserEntity invalidPayer = makeUser(PAYER_ID, UserEntity.UserType.MERCHANT, ENI);
+
+        when(userGateway.findUserById(transferenceEntity.getPayerId())).thenReturn(invalidPayer);
+
+        Either<Error, Object> result = sut.execute(transferenceEntity);
+
+        assertTrue(result.isLeft());
+        assertInstanceOf(MerchantPayerError.class, result.getLeft());
     }
 
     TransferenceEntity makeEntity() {
-        return new TransferenceEntity(BigDecimal.valueOf(100), 1L, 2L);
+        return new TransferenceEntity(BigDecimal.valueOf(100), PAYER_ID, PAYEE_ID);
+    }
+
+    UserEntity makeUser(Long id, UserEntity.UserType userType, String document) {
+        return new UserEntity(id, "any_name", document, "any_email@mail.com", "any_password", userType, BigDecimal.valueOf(100), null, null, null);
     }
 
 }
