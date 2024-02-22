c632037c53b783a157e457caf1b2d0496ef81ff8
Willian Vassilek <willz3@outlook.com>
2024-02-22 16:27:16 -0300

    feat: TransferenceController

--- src/main/java/com/example/banktransfer/module/transference/v1/controller/TransferenceController.java
+++ src/main/java/com/example/banktransfer/module/transference/v1/controller/TransferenceController.java
@@ -1,8 +1,11 @@
 package com.example.banktransfer.module.transference.v1.controller;
 
+import com.example.banktransfer.core.shared.logic.Either;
 import com.example.banktransfer.module.transference.v1.dto.request.CreateTransferenceDTO;
 import com.example.banktransfer.module.transference.v1.entity.TransferenceEntity;
 import com.example.banktransfer.module.transference.v1.mapper.presentation.CreateTransferenceMapper.ICreateTransferenceMapper;
+import com.example.banktransfer.module.transference.v1.usecase.CreateTransference.ICreateTransferenceUseCase;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +19,23 @@ public class TransferenceController {
 
     private final ICreateTransferenceMapper mapper;
 
-    public TransferenceController(ICreateTransferenceMapper mapper) {
+    private final ICreateTransferenceUseCase useCase;
+
+    public TransferenceController(ICreateTransferenceMapper mapper, ICreateTransferenceUseCase useCase) {
         this.mapper = mapper;
+        this.useCase = useCase;
     }
 
     @PostMapping("/")
     public ResponseEntity<Object> handle(@RequestBody @Validated CreateTransferenceDTO request) {
         TransferenceEntity transference = mapper.toEntity(request);
 
-        return ResponseEntity.noContent().build();
+        Either<Error, TransferenceEntity> result = useCase.execute(transference);
+
+        if (result.isLeft()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result.getLeft().getMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(result.getRight());
     }
 }
