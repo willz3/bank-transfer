57113458e7ae20ab2d11c0f8657b3efcaf39e2c1
Willian Vassilek <willz3@outlook.com>
2024-02-26 15:23:56 -0300

    test: should return 201 if use case execution succeeds

--- src/test/java/com/example/banktransfer/module/transference/v1/controller/TransferenceControllerTest.java
+++ src/test/java/com/example/banktransfer/module/transference/v1/controller/TransferenceControllerTest.java
@@ -2,6 +2,7 @@ package com.example.banktransfer.module.transference.v1.controller;
 
 import com.example.banktransfer.core.shared.logic.Either;
 import com.example.banktransfer.module.transference.v1.dto.request.CreateTransferenceRequestDTO;
+import com.example.banktransfer.module.transference.v1.dto.response.CreateTransferenceResponseDTO;
 import com.example.banktransfer.module.transference.v1.entity.TransferenceEntity;
 import com.example.banktransfer.module.transference.v1.error.PayerNotFoundError;
 import com.example.banktransfer.module.transference.v1.error.UnauthorizedTransactionError;
@@ -16,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -26,7 +28,11 @@ class TransferenceControllerTest {
 
     private TransferenceEntity ENTITY_MOCK = makeEntity();
 
-    private CreateTransferenceRequestDTO DTO = makeDTO();
+    private CreateTransferenceRequestDTO REQUEST_DTO = makeRequestDTO();
+
+    private CreateTransferenceResponseDTO RESPONSE_DTO = makeResponseDTO();
+
+    private Date MOCK_DATE = new Date();
 
     @Mock
     private ICreateTransferenceMapper mapper;
@@ -41,21 +47,22 @@ class TransferenceControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         when(useCase.execute(ENTITY_MOCK)).thenReturn(Either.Right(ENTITY_MOCK));
-        when(mapper.toEntity(DTO)).thenReturn(ENTITY_MOCK);
+        when(mapper.toEntity(REQUEST_DTO)).thenReturn(ENTITY_MOCK);
+        when(mapper.toResponse(ENTITY_MOCK)).thenReturn(RESPONSE_DTO);
     }
 
     @Test
     @DisplayName("should call mapper with correct values")
     void callMapperWithCorrectValues() {
-        sut.handle(DTO);
+        sut.handle(REQUEST_DTO);
 
-        verify(mapper, times(1)).toEntity(DTO);
+        verify(mapper, times(1)).toEntity(REQUEST_DTO);
     }
 
     @Test
     @DisplayName("should call use case with correct values")
     void callUseCaseWithCorrectValues() {
-        CreateTransferenceRequestDTO dto = makeDTO();
+        CreateTransferenceRequestDTO dto = makeRequestDTO();
         when(mapper.toEntity(dto)).thenReturn(ENTITY_MOCK);
 
         sut.handle(dto);
@@ -66,7 +73,7 @@ class TransferenceControllerTest {
     @Test
     @DisplayName("should return 400 if use case execution return PayerNotFoundError.")
     void returnBadRequest() {
-        CreateTransferenceRequestDTO dto = makeDTO();
+        CreateTransferenceRequestDTO dto = makeRequestDTO();
 
         PayerNotFoundError error = new PayerNotFoundError();
         when(useCase.execute(ENTITY_MOCK)).thenReturn(Either.Left(error));
@@ -80,17 +87,18 @@ class TransferenceControllerTest {
     @Test
     @DisplayName("should return 201 if use case execution succeeds.")
     void returnCrated() {
-        CreateTransferenceRequestDTO dto = makeDTO();
+        CreateTransferenceRequestDTO dto = makeRequestDTO();
 
         ResponseEntity<Object> result = sut.handle(dto);
 
         assertEquals(result.getStatusCode().value(), 201);
+        assertEquals(result.getBody(), RESPONSE_DTO);
     }
 
     @Test
     @DisplayName("should return 401 if use case return UnauthorizedTransactionError.")
     void returnUnauthorized() {
-        CreateTransferenceRequestDTO dto = makeDTO();
+        CreateTransferenceRequestDTO dto = makeRequestDTO();
         UnauthorizedTransactionError error = new UnauthorizedTransactionError();
         when(useCase.execute(ENTITY_MOCK)).thenReturn(Either.Left(error));
         ResponseEntity<Object> result = sut.handle(dto);
@@ -99,8 +107,12 @@ class TransferenceControllerTest {
         assertEquals(result.getBody(), error.getMessage());
     }
 
-    CreateTransferenceRequestDTO makeDTO() {
+    CreateTransferenceRequestDTO makeRequestDTO() {
         return new CreateTransferenceRequestDTO(BigDecimal.valueOf(100), 1L, 2L);
+    }
+
+    CreateTransferenceResponseDTO makeResponseDTO() {
+        return new CreateTransferenceResponseDTO(1L, BigDecimal.valueOf(100), 1L, 2L, MOCK_DATE);
     }
 
     TransferenceEntity makeEntity() {
